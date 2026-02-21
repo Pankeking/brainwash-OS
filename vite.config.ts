ae814411a3c9caeaad6658d298b59d270d429ce2
Javier Fernandez <javier.fernandez@check24.de>
2026-02-21 21:48:28 +0100

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
 export default defineConfig({
   server: {
@@ -11,6 +12,7 @@ export default defineConfig({
   },
   plugins: [
     tailwindcss(),
+    nitro(),
     tsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
